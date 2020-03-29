b3fff44f37aa246518122816a7776ef2d6c7a8d9
shimamu <cooklecurry@gmail.com>
2020-03-29 15:36:52 +0900

    Delete comment out.

--- setup_urxvt.sh
+++ setup_urxvt.sh
@@ -13,11 +13,11 @@ EOL
 
 # Process
 # ==============================================================================
-#apt_update_only_first_time
-#sudo apt install -y rxvt-unicode-256color fonts-ipafont
+apt_update_only_first_time
+sudo apt install -y rxvt-unicode-256color fonts-ipafont
 #svn export https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/Hack ~/.fonts/Hack
 svn export https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/CodeNewRoman ~/.fonts/CodeNewRoman
-#git clone https://github.com/shimamu/x-dotfiles.git ${CONF_DIR}/x-dotfiles
-#pushd ${CONF_DIR}/x-dotfiles
-#bash setup.sh
-#popd
+git clone https://github.com/shimamu/x-dotfiles.git ${CONF_DIR}/x-dotfiles
+pushd ${CONF_DIR}/x-dotfiles
+bash setup.sh
+popd
