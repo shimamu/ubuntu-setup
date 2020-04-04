76c49b62bbd55ecb689a5436fa57b5bd71d7bb83
shimamu <cooklecurry@gmail.com>
2020-04-04 13:48:48 +0900

    Set apt install package 'suckless-tools' for dmenu.

--- setup_xmonad.sh
+++ setup_xmonad.sh
@@ -14,7 +14,7 @@ EOL
 # Process
 # ==============================================================================
 apt_update_only_first_time
-sudo apt install -y xmonad xmobar feh compton
+sudo apt install -y xmonad xmobar feh compton suckless-tools
 git clone https://github.com/shimamu/xmonad-dotfiles.git ${CONF_DIR}/xmonad-dotfiles
 pushd ${CONF_DIR}/xmonad-dotfiles
 bash setup.sh
